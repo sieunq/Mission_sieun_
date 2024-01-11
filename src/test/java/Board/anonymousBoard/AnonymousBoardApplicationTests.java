d9f97edbb4a6e4240a524ac8808891d2d2aae3ef
jeeho.dev <qufcjfja2021@gmail.com>
2024-01-11 15:01:02 +0900

    first commit

---
+++ src/test/java/Board/anonymousBoard/AnonymousBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Board.anonymousBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
